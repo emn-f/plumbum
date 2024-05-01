85a0db70907265a489b271db930abba72a6f448c
Emanuel <emnarlanferreira@gmail.com>
2024-05-01 17:16:37 -0300

    Subindo últimas alterações.

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:8888/plumbum-api",
+  baseURL: "http://localhost:80/plumbum-api",
   headers: {
     "Content-type": "application/json"
   }
